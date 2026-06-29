18ee14a4ee4aa0a18349decd0f643f88ba3a7ad9
yanivcohen223 <yanivcohen223@gmail.com>
2026-06-29 17:27:49 +0300

    commit

--- db/connection.js
+++ db/connection.js
@@ -5,6 +5,7 @@ exports.dbConnection = {
         try {
             const connection = await mysql.createConnection({
                 host: process.env.DB_HOST,
+                port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306,
                 user: process.env.DB_USERNAME,
                 password: process.env.DB_PASSWORD,
                 database: process.env.DB_NAME,
